4112a1f9df5e73d3316a1756d1aa6441e8e733e2
Ryan Hall <hall.ryan.r@gmail.com>
2021-06-28 16:00:33 -0400

    upgrade to solidity 0.8.6

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import '@typechain/hardhat'
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: '0.8.4',
+  solidity: '0.8.6',
   typechain: {
     outDir: 'typechain',
     target: 'ethers-v5',
